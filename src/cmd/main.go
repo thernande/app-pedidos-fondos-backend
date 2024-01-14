e6e53ac2167ff24199353bbdbcb27a33da53fcd3
TulioH <t.hernandez@abracol.com>
2024-01-14 16:26:28 -0500

    se elimina la carpeta de logs

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/thernande/app-pedidos-fondos-backend/internal/model"
 	"github.com/thernande/app-pedidos-fondos-backend/pkg/appLogs"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--migrate" {
+		model.Migrate()
+		os.Exit(0)
+	}
 	logger := &appLogs.Logger{}
 	logger.Init()
 	logger.InfoLogPrint("Hello World")
-	model.Migrate()
 }
